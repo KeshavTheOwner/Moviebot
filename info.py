2e6fce7d52e88fe9d5da5db24e91c8e3ef2f08fd
KeshavTheOwner <devic534.8@gmail.com>
2025-09-02 12:25:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8211449985').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/KeshavTheOwner # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/KeshavTheOwner") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002939065390'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/movies_request_groups_9')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002908843106').split()]
